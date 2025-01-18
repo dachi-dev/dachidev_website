35f788573d6255806bff9bbb79ec9a90243a1df9
dachi-nodies <dachi@nodies.org>
2025-01-18 08:57:52 -0500

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://dachi-dev.github.io/dachidev_website/"
+  base: "/dachidev_website/"
 })
